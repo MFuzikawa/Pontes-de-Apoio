3881ed15d660c77556733550fa426adbcd5984a4
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-16 11:50:17 -0300

    criação das minhas entidades

---
+++ src/main/java/com/project/Pontes_de_Apoio/domain/entity/Doacao.java
@@ -0,0 +1,4 @@
+package com.project.Pontes_de_Apoio.domain.entity;
+
+public class Doacao {
+}
---
+++ src/main/java/com/project/Pontes_de_Apoio/domain/entity/Doador.java
@@ -0,0 +1,8 @@
+package com.project.Pontes_de_Apoio.domain.entity;
+
+public class Doador {
+    private String id;
+    private String name;
+    private String email;
+    private String number;
+}
