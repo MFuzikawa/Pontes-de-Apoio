6a59f2e39b71e8d9896751b003afd9176a3e5cd4
Fuzikawa <murilopyskfuzikawa@gmail.com>
2025-09-16 11:49:55 -0300

    API de Gestão de Doações
    Problema real: ONGs precisam organizar doações e acompanhar relatórios.
    
    1. Entidades principais
    
    Doacao (id, valor, tipo, data, idDoador)
    
    Doador (id, nome, email, telefone)
    
    2. Relacionamentos
    
    Um Doador pode ter várias Doações (1:N).
    
    3. Endpoints
    
    POST /doadores → cadastrar doador
    
    GET /doadores → listar doadores
    
    POST /doacoes → registrar doação
    
    GET /doacoes → listar todas doações
    
    GET /doacoes/periodo?inicio=...&fim=... → listar por período
    
    GET /doacoes/relatorio → total por mês

---
+++ src/test/java/com/project/Pontes_de_Apoio/PontesDeApoioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.Pontes_de_Apoio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontesDeApoioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
